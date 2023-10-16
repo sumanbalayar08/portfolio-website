21d79f6f2b2d3364a5dcaff11af3e20fa5c5be01
SumanBalayar <orthohydrogen123@gmail.com>
2023-10-16 19:27:26 +0545

    final

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -24,60 +24,67 @@ const Home = () => {
   ];
 
   return (
-      <div id="home" className="flex-col space-y-7 px-[13%] pt-[8%] bg-slate-100 pb-[4%]">
-        <div className="flex space-x-2 justify-between">
-          <div className="flex-col space-y-6">
-            <div className="space-y-2">
-              <p className="text-6xl font-extrabold">Full Stack Software</p>
-
-              <p className="text-6xl font-extrabold">Developer👋</p>
-            </div>
-
-            <p className="text-lg text-slate-700 font-sans leading-8">
-              Hello, I'm Suman Balayar. A passionate Full Stack Software
-              Developer based in Nepal. 📍
-            </p>
-
-            <div className="flex space-x-4">
-            <Link to='https://www.linkedin.com/in/balayar08/'><AiOutlineLinkedin size={30} className="cursor-pointer" /></Link>
-              <Link to='https://github.com/sumanbalayar08'><AiOutlineGithub size={30} className="cursor-pointer" /></Link>
-            </div>
-          </div>
-          <div>
-            <img src={myImage} alt="My Pic" />
-          </div>
+    <div id="home" className="flex-col space-y-7 px-[8%] py-[4%] bg-slate-100">
+      <div className="flex-1 lg:flex lg:flex-row-reverse space-x-2 justify-between">
+        <div className="flex items-center justify-center">
+          <img src={myImage} alt="My Pic" className="w-2/3 lg:w-[55vh]" />
         </div>
-        <div className="flex items-center space-x-20">
-          <div className="flex items-center space-x-4">
-            <span className="text-lg">Tech Stack</span>
-            <div className="h-5 w-1 border-r-2 border-slate-400"></div>
+        <div className="flex-1 space-y-6">
+          <div className="space-y-2 text-center text-4xl lg:text-6xl lg:text-left ">
+            <p className="font-extrabold">Full Stack Software</p>
+            <p className="font-extrabold">Developer👋</p>
           </div>
-          <div className="flex space-x-6">
-            {images
-              .reduce((result, item, index) => {
-                if (index % 2 === 0) {
-                  const pair = [images[index], images[index + 1]];
-                  result.push(pair);
-                }
-                return result;
-              }, [])
-              .map((pair, index) => (
-                <div key={index} className="flex -space-x-1">
-                  <img
-                    src={pair[0]}
-                    alt={`Image ${index} - 1`}
-                    className="w-16 flex"
-                  />
-                  <img
-                    src={pair[1]}
-                    alt={`Image ${index} - 2`}
-                    className="w-16 flex"
-                  />
-                </div>
-              ))}
+
+          <p className="text-lg text-slate-700 font-sans lg:leading-8 text-center lg:text-left">
+            Hello, I'm Suman Balayar. A passionate Full Stack Software Developer
+            based in Nepal.📍
+          </p>
+
+          <div className="flex space-x-4 items-center justify-center lg:justify-start">
+            <Link to="https://www.linkedin.com/in/balayar08/">
+              <AiOutlineLinkedin size={30} className="cursor-pointer" />
+            </Link>
+            <Link to="https://github.com/sumanbalayar08">
+              <AiOutlineGithub size={30} className="cursor-pointer" />
+            </Link>
           </div>
         </div>
       </div>
+
+      <div className="flex-1 items-center space-y-4 lg:space-y-0 lg:flex lg:space-x-8">
+        <div className="flex-initial items-center justify-center lg:flex lg:space-x-2 lg:justify-start">
+          <div className="text-lg text-center">Tech Stack</div>
+          <div className="lg:hidden flex items-center justify-center">
+            <div className="h-1 w-28 border-b-2 border-slate-400"></div>
+          </div>
+          <div className="hidden lg:block h-6 w-1 border-r-2 border-slate-400"></div>
+        </div>
+        <div className="flex space-x-2 md:space-x-4 lg:space-x-6 items-center justify-center">
+          {images
+            .reduce((result, item, index) => {
+              if (index % 2 === 0) {
+                const pair = [images[index], images[index + 1]];
+                result.push(pair);
+              }
+              return result;
+            }, [])
+            .map((pair, index) => (
+              <div key={index} className="flex -space-x-1">
+                <img
+                  src={pair[0]}
+                  alt={`Image ${index} - 1`}
+                  className="w-8 md:w-12 lg:w-16 flex"
+                />
+                <img
+                  src={pair[1]}
+                  alt={`Image ${index} - 2`}
+                  className="w-8 md:w-12 lg:w-16 flex"
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,84 @@
-import React from "react";
-import {Link as ScrollLink} from 'react-scroll'
+import React, { useState } from "react";
+import { Link as ScrollLink } from "react-scroll";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-  return (
-    <div className="flex items-center justify-between shadow-sm py-[1.5%] px-[4%] sticky top-0 bg-white">
-      <div>
-        <span className="text-xl font-semibold">Balayar.DEV</span>
-      </div>
+  const [toggle, settoggle] = useState(false);
 
-      <ul className="flex space-x-4 text-lg font-medium font-sans">
-        <li className="hover:text-sky-600 cursor-pointer"><ScrollLink activeClass="active" to="home" spy={true} smooth={true}>Home</ScrollLink></li>
-        <li className="hover:text-sky-600 cursor-pointer"><ScrollLink to="about" spy={true} smooth={true}>About</ScrollLink></li>
-        <li className="hover:text-sky-600 cursor-pointer"><ScrollLink to="projects" spy={true} smooth={true}>Projects</ScrollLink></li>
-        <li className="hover:text-sky-600 cursor-pointer"><ScrollLink to="contacts" spy={true} smooth={true}>Contacts</ScrollLink></li>
-      </ul>
+  const toggleMenu = () => {
+    settoggle(!toggle);
+  };
+
+  return (
+    <div className="bg-white w-full top-0 left-0 shadow-sm h-[16]">
+      <div className="md:flex items-center justify-between py-4 sticky bg-white md:px-6">
+        <div>
+          <span className="text-xl font-semibold">Balayar.DEV</span>
+        </div>
+
+        <ul className="md:flex space-x-4 text-lg font-medium font-sans hidden">
+          <li className="hover:text-sky-600 cursor-pointer">
+            <ScrollLink activeClass="active" to="home" spy={true} smooth={true}>
+              Home
+            </ScrollLink>
+          </li>
+          <li className="hover:text-sky-600 cursor-pointer">
+            <ScrollLink to="about" spy={true} smooth={true}>
+              About
+            </ScrollLink>
+          </li>
+          <li className="hover:text-sky-600 cursor-pointer">
+            <ScrollLink to="projects" spy={true} smooth={true}>
+              Projects
+            </ScrollLink>
+          </li>
+          <li className="hover:text-sky-600 cursor-pointer">
+            <ScrollLink to="contacts" spy={true} smooth={true}>
+              Contacts
+            </ScrollLink>
+          </li>
+        </ul>
+
+        <ul
+          className={`absolute md:hidden items-center bg-white top-[61px] w-full space-y-4 justify-center transform ${
+            toggle ? "block" : "hidden"
+          }`}
+        >
+          <div className="flex flex-col items-center space-y-4">
+            <li className="hover:text-sky-600 cursor-pointer">
+              <ScrollLink
+                activeClass="active"
+                to="home"
+                spy={true}
+                smooth={true}
+              >
+                Home
+              </ScrollLink>
+            </li>
+            <li className="hover:text-sky-600 cursor-pointer">
+              <ScrollLink to="about" spy={true} smooth={true}>
+                About
+              </ScrollLink>
+            </li>
+            <li className="hover:text-sky-600 cursor-pointer">
+              <ScrollLink to="projects" spy={true} smooth={true}>
+                Projects
+              </ScrollLink>
+            </li>
+            <li className="hover:text-sky-600 cursor-pointer">
+              <ScrollLink to="contacts" spy={true} smooth={true}>
+                Contacts
+              </ScrollLink>
+            </li>
+          </div>
+        </ul>
+
+        <GiHamburgerMenu
+          className="absolute cursor-pointer block md:hidden right-8 top-5 scale-110"
+          onClick={toggleMenu}
+        />
+      </div>
+      <hr className="border-slate-300 border-solid" />
     </div>
   );
 };
